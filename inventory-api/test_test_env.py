8353ddedfd4f17468a8419924804b3e8ce0668ab
khiem2010 <npqkhiem2010@gmail.com>
2025-09-15 18:02:21 +0700

    Refactor: Reorganized test files and ignored Python cache

--- inventory-api/test_test_env.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-env_vars = os.environ.keys()
-print(list(env_vars))
-
-print('--------------------------------------------------------')
-print(os.environ.get('HOME'))
-load_dotenv()
-print('DATABASE_URL' in list(env_vars))
--- inventory-web/test_login.js
+++
@@ -1,6 +0,0 @@
-const response = await fetch (`/login?Username=${username}&Password=${password}`, 
-      {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'}
-      })
-console.log(response.json())
