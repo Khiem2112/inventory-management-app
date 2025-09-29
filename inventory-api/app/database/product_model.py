106e7d0c14ab5264d8733a6e9b18c39bb33ba250
khiem2010 <npqkhiem2010@gmail.com>
2025-09-29 22:27:29 +0700

    BE: Elaborate product schema

--- inventory-api/app/database/product_model.py
+++ inventory-api/app/database/product_model.py
@@ -1,7 +1,7 @@
 # In app/database/DBModel.py
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Float, Integer, DateTime
+from sqlalchemy import String, Float, Integer, DateTime, FLOAT
 from datetime import datetime
 
 # You'll need to update your base class to inherit from DeclarativeBase
@@ -12,12 +12,34 @@ class Product(Base):
   __tablename__ = "Product"
 
   # Use mapped_column for each column definition
+  
+  # Product Identity
   ProductId: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+  ModelNumber_SKU: Mapped[str] = mapped_column(String, nullable=False)
   ProductName: Mapped[str] = mapped_column(String(255), nullable=False)
+  
+  # General info
+  Category: Mapped[str] = mapped_column(String, nullable=False)
+  ProductSeries: Mapped[str] = mapped_column(String, nullable=False)
+  Manufacturer: Mapped[str] = mapped_column(String, nullable=True)
   Measurement: Mapped[str] = mapped_column(String(255), nullable=False)
   SellingPrice: Mapped[float] = mapped_column(Float) # DECIMAL maps to Float in Pydantic
-  InternalPrice: Mapped[float] = mapped_column(Float, nullable=False)    
+  InternalPrice: Mapped[float] = mapped_column(Float, nullable=False)
+  
+  # Media
   ProductImageId: Mapped[str] = mapped_column(String, nullable=True)
   ProductImageUrl: Mapped[str] = mapped_column(String, nullable=True)
+  
+  # Technical stats
+  PackageWeight_KG: Mapped[float] = mapped_column(FLOAT, nullable=True)
+  Dimensions_H_CM: Mapped[float] = mapped_column(FLOAT, nullable=True)
+  Dimensions_W_CM: Mapped[float] = mapped_column(FLOAT, nullable=True)
+  Dimensions_D_CM: Mapped[float] = mapped_column(FLOAT, nullable=True)
+  
+  # Additional
+  SafetyStock: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+  WarrantyPeriod_Days: Mapped[int] = mapped_column(Integer, nullable=True, default=0)
+  PrimarySupplierID :  Mapped[int] = mapped_column(Integer, nullable=True)
+  
   class Config:
     from_attributes = True
