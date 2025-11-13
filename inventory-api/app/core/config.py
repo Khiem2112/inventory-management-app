677e7bd9638d3a27928b2eefa665298bb0458326
khiem2010 <npqkhiem2010@gmail.com>
2025-11-13 08:47:54 +0700

    Fixed variable loading in env, now it can load the passed env variables from outside of container

--- inventory-api/app/core/config.py
+++ inventory-api/app/core/config.py
@@ -7,9 +7,13 @@ import cloudinary
 # This makes sure dotenv finds the .env file regardless of where this script is called from.
 # It navigates up from app/core/ to app/ to inventory-api/ and looks for .env there.
 from dotenv import load_dotenv
+IS_PRODUCTION = os.environ.get('K_SERVICE') is not None
 
-env_dir = os.path.dirname(os.path.dirname(__file__))
-env_path = os.path.join(env_dir,'.env')
+# Only load the .env file if we are NOT in production
+if not IS_PRODUCTION:
+    env_dir = os.path.dirname(os.path.dirname(__file__))
+    env_path = os.path.join(env_dir,'.env')
+    load_dotenv(env_path)
 
 load_dotenv(env_path)
 
@@ -27,7 +31,7 @@ class Settings(BaseSettings):
 
     # Configuration for BaseSettings itself
     model_config = SettingsConfigDict(
-        env_file='.env', # Look for variables in .env file
+        env_file=None, # Look for variables in .env file
         extra='ignore'   # Ignore variables in .env that are not defined in this class
     )
 
